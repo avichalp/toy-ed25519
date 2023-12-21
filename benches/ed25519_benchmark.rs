607802f6e3929aa2fe54b72c45baca80c2e40e8a
avichalp <hi@avichalp.me>
2023-12-21 14:28:54 +0800

    refactor and add benchmark tests
    Signed-off-by: avichalp <hi@avichalp.me>

---
+++ benches/ed25519_benchmark.rs
@@ -0,0 +1,96 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use ed25519::FieldElement;
+
+fn bench_inverse(c: &mut Criterion) {
+    let mut items = [0; 32];
+    // force last byte to be less than 128
+    // so that the MSB is 0. This is because
+    // p = 2^255-19. we only allow numbers
+    // in [0,2^255] (see unpack docs)
+    items[31] = 0x2;
+    let packed = FieldElement::new(items);
+    let unpacked = packed.unpack();
+
+    c.bench_function("inverse", |b| b.iter(|| unpacked.inverse()));
+}
+
+fn bench_add(c: &mut Criterion) {
+    let mut a = [0; 32];
+    a[31] = 0x2;
+    let packed_a = FieldElement::new(a);
+
+    let mut b = [0; 32];
+    b[31] = 0x2;
+    let packed_b = FieldElement::new(b);
+
+    let unpacked_a = packed_a.unpack();
+    let unpacked_b = packed_b.unpack();
+
+    c.bench_function("add", |b| b.iter(|| unpacked_a.add(&unpacked_b)));
+}
+
+fn bench_mul(c: &mut Criterion) {
+    let mut a = [0; 32];
+    a[31] = 0x2;
+    let packed_a = FieldElement::new(a);
+
+    let mut b = [0; 32];
+    b[31] = 0x2;
+    let packed_b = FieldElement::new(b);
+
+    let unpacked_a = packed_a.unpack();
+    let unpacked_b = packed_b.unpack();
+
+    c.bench_function("mul", |b| b.iter(|| unpacked_a.mul(&unpacked_b)));
+}
+
+fn bench_sub(c: &mut Criterion) {
+    let mut a = [0; 32];
+    a[31] = 0x2;
+    let packed_a = FieldElement::new(a);
+
+    let mut b = [0; 32];
+    b[31] = 0x2;
+    let packed_b = FieldElement::new(b);
+
+    let unpacked_a = packed_a.unpack();
+    let unpacked_b = packed_b.unpack();
+
+    c.bench_function("sub", |b| b.iter(|| unpacked_a.sub(&unpacked_b)));
+}
+
+fn bench_unpack(c: &mut Criterion) {
+    let mut items = [0; 32];
+    // force last byte to be less than 128
+    // so that the MSB is 0. This is because
+    // p = 2^255-19. we only allow numbers
+    // in [0,2^255] (see unpack docs)
+    items[31] = 0x2;
+    let packed = FieldElement::new(items);
+
+    c.bench_function("unpack", |b| b.iter(|| packed.unpack()));
+}
+
+fn bench_pack(c: &mut Criterion) {
+    let mut items = [0; 32];
+    // force last byte to be less than 128
+    // so that the MSB is 0. This is because
+    // p = 2^255-19. we only allow numbers
+    // in [0,2^255] (see unpack docs)
+    items[31] = 0x2;
+    let packed = FieldElement::new(items);
+    let mut unpacked = packed.unpack();
+
+    c.bench_function("pack", |b| b.iter(|| unpacked.pack()));
+}
+
+criterion_group!(
+    benches,
+    bench_inverse,
+    bench_add,
+    bench_mul,
+    bench_sub,
+    bench_unpack,
+    bench_pack
+);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
 use std::i64;
 
-#[derive(Debug, Clone, PartialEq)]
-struct FieldElement<T, const SIZE: usize> {
+use proptest::bits::usize;
+
+#[derive(Debug, Clone)]
+pub struct FieldElement<T, const SIZE: usize> {
     items: [T; SIZE],
 }
 
-impl<T, const SIZE: usize> FieldElement<T, SIZE> {
-    fn len(&self) -> usize {
-        SIZE
+impl<T: Default + Copy, const SIZE: usize> Default for FieldElement<T, SIZE> {
+    fn default() -> Self {
+        Self {
+            items: [T::default(); SIZE],
+        }
     }
 }
 
 impl FieldElement<u8, 32> {
-    fn new(items: [u8; 32]) -> Self {
+    pub fn new(items: [u8; 32]) -> Self {
         Self { items }
     }
 
@@ -23,37 +27,36 @@ impl FieldElement<u8, 32> {
     // always less than 2^255 (2^255-19, but we allow [2^255-19, 2^255-1]).
     // We could have used u16 instead of i64 theorectically, i64 prevents
     // any possible overflow/underflow.
-    fn unpack(&self) -> FieldElement<i64, 16> {
-        let mut unpacked = FieldElement { items: [0; 16] };
-        for i in 0..16 {
-            let byte1 = self.items[2 * i] as i64;
-            let byte2 = self.items[(2 * i) + 1] as i64;
-            unpacked.items[i] = (byte2 << 8) + byte1;
-        }
+    pub fn unpack(&self) -> FieldElement<i64, 16> {
+        let mut unpacked = FieldElement::default();
+        self.items.chunks(2).enumerate().for_each(|(i, chunk)| {
+            unpacked.items[i] = ((chunk[1] as i64) << 8) + chunk[0] as i64;
+        });
         unpacked.items[15] = unpacked.items[15] & 0x7fff;
         unpacked
     }
 }
 
 impl FieldElement<i64, 16> {
-    fn add(&self, other: &Self) -> Self {
-        let mut result = Self { items: [0; 16] };
-        for i in 0..16 {
-            result.items[i] = self.items[i] + other.items[i];
-        }
+    pub fn add(&self, other: &Self) -> Self {
+        let mut result = FieldElement::default();
+        result.items.iter_mut().enumerate().for_each(|(i, item)| {
+            *item = self.items[i] + other.items[i];
+        });
+
         result
     }
 
-    fn sub(&self, other: &Self) -> Self {
-        let mut result = Self { items: [0; 16] };
-        for i in 0..16 {
-            result.items[i] = self.items[i] - other.items[i];
-        }
+    pub fn sub(&self, other: &Self) -> Self {
+        let mut result = FieldElement::default();
+        result.items.iter_mut().enumerate().for_each(|(i, item)| {
+            *item = self.items[i] - other.items[i];
+        });
+
         result
     }
 
-    fn mul(&self, other: &Self) -> Self {
-        // todo: impl a `product` method for [i64; 32]
+    pub fn mul(&self, other: &Self) -> Self {
         let mut product: [i64; 32] = [0; 32];
         for i in 0..16 {
             for j in 0..16 {
@@ -64,12 +67,13 @@ impl FieldElement<i64, 16> {
             product[i] += 38 * product[i + 16];
         }
 
-        let mut result = Self { items: [0; 16] };
-        for i in 0..16 {
-            result.items[i] = product[i];
-        }
+        let mut result = FieldElement::default();
+        result.items.iter_mut().enumerate().for_each(|(i, item)| {
+            *item = product[i];
+        });
         result.carry();
         result.carry();
+
         result
     }
 
@@ -81,30 +85,28 @@ impl FieldElement<i64, 16> {
     // => 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeb
     // All the bits of p-2 are 1 except for the 2nd and 4th bits.
     //
-    // The loop in the f inverse function counts down from the
+    // The loop in the inverse function counts down from the
     // most-significant to the least-significant bit, squaring
-    // the current value c using the fmul function for each bit,
-    // and also multipling c with the input value in for each bit that is 1.
-    // Even though p=2 consists of 255 bits, the loop is able to start
-    // at bit 253 and save one iteration by initialising c to in instead of 1.
-    // At the end, c is copied to the output variable out.
-    fn inverse(&self) -> Self {
-        // println!("mod inversing");
-        let mut temp = self.clone();
-        // let mut result = self.clone();
+    // the current value for each bit, and also multipling the
+    // result with the input value in for each bit that is 1.
+    // Even though p=2 consists of 255 bits, the loop is able to
+    // start at bit 253 and save one iteration by initialising
+    // the result to in instead of 1.
+    pub fn inverse(&self) -> Self {
+        let mut result = self.clone();
         for i in (0..=253).rev() {
-            // println!("temp == {:?}, {:?}", temp, i);
-            temp = temp.mul(&temp);
+            result = result.mul(&result);
             if i != 2 && i != 4 {
-                temp = temp.mul(&self);
+                result = result.mul(&self);
             }
         }
-        temp
+
+        result
     }
 
     // If b is 1 and bits in p and q differ, swap the bits in p and q.
     // If b is 0, do nothing. If the bits are the same, do nothing.
-    fn swap(&mut self, other: &mut Self, b: i64) {
+    pub fn swap(&mut self, other: &mut Self, b: i64) {
         let c = !(b - 1);
         for i in 0..16 {
             let t = c & (self.items[i] ^ other.items[i]);
@@ -118,10 +120,12 @@ impl FieldElement<i64, 16> {
     // If there is a carry, the carry is subtracted from the current element
     // and added to the next element. If the current element is the last element,
     // the carry is multiplied by 38 (19 * 2) and added to the first element.
-    fn carry(&mut self) {
+    pub fn carry(&mut self) {
         for i in 0..16 {
-            let carry = self.items[i] >> 16; // 1. divide by 2^16
-            self.items[i] -= carry << 16; // 2. multiply by 2^16 and subtract
+            // 1. divide by 2^16
+            let carry = self.items[i] >> 16;
+            // 2. multiply by 2^16 and subtract
+            self.items[i] -= carry << 16;
             if i < 15 {
                 self.items[i + 1] += carry;
             } else {
@@ -130,32 +134,37 @@ impl FieldElement<i64, 16> {
         }
     }
 
-    fn pack(&mut self) -> FieldElement<u8, 32> {
-        let mut temp = Self { items: [0; 16] };
+    pub fn pack(&mut self) -> FieldElement<u8, 32> {
+        let mut temp = FieldElement::default();
         self.carry();
         self.carry();
         self.carry();
+        // except for the first 16 and last 16 bits all the bits are 1
         for _ in 0..2 {
-            // 0xffed are the least significant 16 bits of 2^255-19
-            // except for the first 16 and last 16 bits all the bits are 1
+            // 0xffed are the least significant 16 bits of 2^255-19.
+            // subtract them from first item of the field element array
             temp.items[0] = self.items[0] - 0xffed;
             for i in 1..15 {
+                // subtract 0xffff from intermediate items and also check if there is a carry
+                // by checking if i-1th item exceeds 2^16, if it does subtract the carry bits too
+                // and adjust the ith item accordingly by taking mod 2^16
                 temp.items[i] = self.items[i] - 0xffff - ((temp.items[i - 1] >> 16) & 1);
                 temp.items[i - 1] &= 0xffff;
             }
-            // 0x7fff are the most significant 16 bits of 2^255-19
+            // 0x7fff are the most significant 16 bits of 2^255-19, subtract them from the last item
+            // of the field element array and also check if there is a carry by checking if the
+            // 14th item exceeds 2^16, if it does subtract the carry bits too and adjust the 15th
             temp.items[15] = self.items[15] - 0x7fff - ((temp.items[14] >> 16) & 1);
             let carry = (temp.items[15] >> 16) & 1;
             temp.items[14] &= 0xffff;
             self.swap(&mut temp, 1 - carry);
         }
 
-        let mut result = FieldElement { items: [0; 32] };
+        let mut result = FieldElement::default();
         for i in 0..16 {
             result.items[2 * i] = (self.items[i] & 0xff) as u8;
             result.items[(2 * i) + 1] = (self.items[i] >> 8) as u8;
         }
-
         result
     }
 }
@@ -179,7 +188,7 @@ mod tests {
 
             let repacked = unpacked.pack();
 
-            assert_eq!(packed, repacked);
+            assert_eq!(packed.items, repacked.items);
         }
     }
 
@@ -204,7 +213,7 @@ mod tests {
             let unpacked_b = packed_b.unpack();
             let unpacked_c = unpacked_a.add(&unpacked_b);
 
-            assert_eq!(unpacked_a, unpacked_c.sub(&unpacked_b));
+            assert_eq!(unpacked_a.items, unpacked_c.sub(&unpacked_b).items);
         }
     }
 
@@ -229,7 +238,7 @@ mod tests {
             let mut expected = FieldElement::new([0; 32]);
             expected.items[0] = 1 as u8;
 
-            assert_eq!(expected, packed_c);
+            assert_eq!(expected.items, packed_c.items);
         }
     }
 }
